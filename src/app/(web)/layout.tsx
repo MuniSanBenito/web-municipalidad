85172ccda0560b073d8db5d2486b23d125b334f2
Adriano <adricatena@gmail.com>
2025-02-21 10:03:17 -0300

    feat: add metadata for page title in layout component

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,13 +1,15 @@
+import type { Metadata } from 'next'
 import type { PropsWithChildren } from 'react'
 import { Navbar } from '../../components/navbar'
 import './globals.css'
 
+export const metadata: Metadata = {
+  title: 'Municipalidad de San Benito',
+}
+
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <html lang="es" data-theme="sanbenito">
-      <head>
-        <title>Municipalidad de San Benito</title>
-      </head>
       <body className="p-0">
         <Navbar />
         <main className="container mx-auto mt-35">{children}</main>
