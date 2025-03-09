d3381f7de89cff1d29c37904a113b045ee61aac9
Adriano <adricatena@gmail.com>
2025-03-09 19:12:05 -0300

    feat: implement access control for create, read, update, and delete operations in Curriculums collection

--- src/collections/Curriculums.ts
+++ src/collections/Curriculums.ts
@@ -1,5 +1,8 @@
+import { accessCreate, accessDelete, accessRead, accessUpdate } from '@/globals/Permisos'
 import type { Curriculum } from '@/payload-types'
 import type { CollectionBeforeChangeHook, CollectionConfig } from 'payload'
+
+const SLUG = 'curriculums'
 
 const beforeChange: CollectionBeforeChangeHook<Curriculum> = async ({ data, req }) => {
   let { user } = data
@@ -15,7 +18,7 @@ const beforeChange: CollectionBeforeChangeHook<Curriculum> = async ({ data, req 
 }
 
 export const Curriculums: CollectionConfig = {
-  slug: 'curriculums',
+  slug: SLUG,
   labels: {
     singular: 'Curriculum',
     plural: 'Curriculums',
@@ -25,6 +28,12 @@ export const Curriculums: CollectionConfig = {
   },
   hooks: {
     beforeChange: [beforeChange],
+  },
+  access: {
+    create: async (args) => await accessCreate({ ...args, collection: SLUG }),
+    read: async (args) => await accessRead({ ...args, collection: SLUG }),
+    update: async (args) => await accessUpdate({ ...args, collection: SLUG }),
+    delete: async (args) => await accessDelete({ ...args, collection: SLUG }),
   },
   fields: [
     {
