aba5fe4d3c3aa0192fa333594ed5414ab232e955
Adriano <adricatena@gmail.com>
2025-03-11 20:18:35 -0300

    feat: simplify roles array by removing redundant role entries

--- src/constants/roles.ts
+++ src/constants/roles.ts
@@ -3,13 +3,7 @@ export const ROL_CIUDADANO_VALUE = 'CIUDADANO'
 export const ROL_PUBLICO_VALUE = 'PUBLICO'
 export const ROL_OWN_VALUE = 'A SI MISMO'
 
-export const ROLES = [
-  ROL_ADMIN_VALUE,
-  ROL_CIUDADANO_VALUE,
-  ROL_PUBLICO_VALUE,
-  ROL_OWN_VALUE,
-  'COMUNICACION',
-] as const
+export const ROLES = [ROL_ADMIN_VALUE, ROL_CIUDADANO_VALUE, 'COMUNICACION'] as const
 export type Rol = (typeof ROLES)[number]
 
 export const ROL_DEFAULT_VALUE: Rol = 'CIUDADANO'
