e57cf88e78c73b567528bccd6d90e3ebfe597701
Adriano <adricatena@gmail.com>
2025-04-28 10:12:10 -0300

    feat: replace Navbar with RootLayout component in Layout for improved structure

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from '@/components/navbar'
+import { RootLayout } from '@/web/components/root-layout'
 import type { Metadata } from 'next'
 import { ThemeProvider } from 'next-themes'
 import type { PropsWithChildren } from 'react'
@@ -11,10 +11,13 @@ export const metadata: Metadata = {
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <body className="p-0">
-        <ThemeProvider themes={['sanbenito', 'sanbenito-dark']}>
-          <Navbar />
-          <div className="container mx-auto mt-35">{children}</div>
+      <head>
+        <meta name="apple-mobile-web-app-title" content="San Benito" />
+      </head>
+      <body className="drawer scroll-smooth">
+        <ThemeProvider>
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <RootLayout>{children}</RootLayout>
         </ThemeProvider>
       </body>
     </html>
