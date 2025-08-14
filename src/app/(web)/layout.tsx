7606a8ffe7914378e0919f8ae8f819c94d629d0b
Adriano <adricatena@gmail.com>
2025-08-14 15:40:24 -0300

    feat: enhance Layout component with authentication handling and user context

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,14 +1,22 @@
 import { RootLayout } from '@/web/components/root-layout'
+import { basePayload } from '@/web/lib/payload'
 import type { Metadata } from 'next'
 import { ThemeProvider } from 'next-themes'
+import { headers as nextHeaders } from 'next/headers'
 import type { PropsWithChildren } from 'react'
+import { Toaster } from 'sonner'
 import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Municipalidad de San Benito',
 }
 
-export default function Layout({ children }: PropsWithChildren) {
+export default async function Layout({ children }: PropsWithChildren) {
+  const headers = await nextHeaders()
+  const result = await basePayload.auth({ headers, canSetHeaders: false })
+
+  console.log('Usuario autenticado:', result.user)
+
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
@@ -17,7 +25,10 @@ export default function Layout({ children }: PropsWithChildren) {
       <body className="drawer scroll-smooth">
         <ThemeProvider>
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-          <RootLayout>{children}</RootLayout>
+          <RootLayout ciudadano={result.user?.collection === 'ciudadanos' ? result.user : null}>
+            {children}
+          </RootLayout>
+          <Toaster richColors closeButton />
         </ThemeProvider>
       </body>
     </html>
